ddbffb609b59a3f60c5678edb15721f11de87907
jsvanilla <36719905+jsvanilla@users.noreply.github.com>
2023-05-04 17:04:05 -0600

    FEATURE node js app from github

--- main.tf
+++ main.tf
@@ -68,12 +68,33 @@ resource "aws_instance" "name" {
 
   user_data = <<-EOF
     <powershell>
+      # Instalación de .NET Framework environment & SQL Server 2019 Developer Edition
       Install-WindowsFeature -Name "NET-Framework-45-Features" -IncludeAllSubFeature
       Install-WindowsFeature -Name "NET-Framework-45-Core"
       [System.Environment]::SetEnvironmentVariable("ACCEPT_EULA", "Y", "Machine")
       Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?linkid=866662" -OutFile "SQLServer2019-DEV-x64-ENU.exe"
       Start-Process -FilePath "SQLServer2019-DEV-x64-ENU.exe" -ArgumentList "/Q", "/IACCEPTPYTHONLICENSETERMS", "/IACCEPTROPENLICENSETERMS", "/IACCEPTSQLSERVERLICENSETERMS", "/ACTION=install", "/FEATURES=SQLEngine,FullText,DQ,BC,Conn", "/INSTANCENAME=MSSQLSERVER", "/SECURITYMODE=SQL", "/SAPWD=MyStr0ngP@ssw0rd", "/TCPENABLED=1", "/UPDATEENABLED=False" -Wait
       Remove-Item "SQLServer2019-DEV-x64-ENU.exe"
+
+      # Instalar Node.js
+      Invoke-WebRequest -Uri "https://nodejs.org/dist/v14.18.1/node-v14.18.1-x64.msi" -OutFile "node-v14.18.1-x64.msi"
+      Start-Process -FilePath "node-v14.18.1-x64.msi" -ArgumentList "/quiet" -Wait
+      Remove-Item "node-v14.18.1-x64.msi"
+
+      # Instalar Git
+      Invoke-WebRequest -Uri "https://github.com/git-for-windows/git/releases/download/v2.34.0.windows.1/Git-2.34.0-64-bit.exe" -OutFile "Git-2.34.0-64-bit.exe"
+      Start-Process -FilePath "Git-2.34.0-64-bit.exe" -ArgumentList "/VERYSILENT", "/NORESTART", "/NOCANCEL", "/SP-", "/CLOSEAPPLICATIONS", "/RESTARTAPPLICATIONS", "/NOICONS", "/COMPONENTS=\"icons,ext,ext\\reg\\shellhere,assoc,assoc_sh\"" -Wait
+      Remove-Item "Git-2.34.0-64-bit.exe"
+
+      # Clonar el repositorio con la aplicación CRUD Node.js
+      git clone https://github.com/jsvanilla/node-sql-server.git C:\\node-sql-server
+
+      # Cambiar al directorio de la aplicación e instalar las dependencias
+      Set-Location -Path C:\\node-sql-server
+      npm install
+
+      # Iniciar la aplicación Node.js en segundo plano
+      Start-Process -FilePath "node" -ArgumentList "app.js" -WindowStyle Hidden
     </powershell>
   EOF
 }
